24ee0b28b8e7bd6b5531caec6c67ebbf7b71b48e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-02-23 01:02:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,23 @@
 var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray( arr, element) {
+  return ( [ element, ... arr])
+}
+
+function destructivelyAddElementToBeginningOfArray( arr, element ) {
+  arr.unshift(element);
+  return arr;
+}
+
+function accessElementInArray(arr, index) {
+  return arr[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray (arr) {
+  return arr.slice(1);
+}
